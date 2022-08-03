a4fd0332341cc19812b44c8f192cedffad0598e2
MbPCM <m9jbhakar@gmail.com>
2022-08-03 23:28:00 +0530

    Working on Smali Syntax Checker

--- src/Experiment.java
+++ src/Experiment.java
@@ -3,6 +3,10 @@ import mbpcm.ui.IButton;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Experiment {
 
@@ -12,28 +16,93 @@ public class Experiment {
 
     }
     Experiment(){
-        String testImage = "start.png";
-        JFrame f = getJFrame("Test");
-        JPanel panel = getHPanel();
-        panel.add(new IButton(utils.getImageFromRes(testImage),"Run"));
-        panel.add(new IButton(null,"RUN"));
-        panel.add(new IButton(utils.getImageFromRes(testImage)));
-        f.add(panel,BorderLayout.NORTH);
-        f.setVisible(true);
+        //String file = "H:\\splitApks\\MoviesFree [1.2]-split\\base\\smali_classes4\\com\\google\\android\\gms\\measurement\\internal\\zzkn.smali";
+        //String file = "H:\\splitApks\\Send Files To TV [1.2.2]-split\\base\\smali\\com\\google\\android\\gms\\internal\\measurement\\zzjk.smali";
+        String file = "G:\\portableApps\\Hacking & Programming\\APK Easy Tool portable\\1-Decompiled APKs\\base\\smali\\androidx\\exifinterface\\media\\ExifInterface.smali";
+        new smaliParser(file);
     }
-    JFrame getJFrame(String name){
-        JFrame f = new JFrame("hello");
-        f.setSize(new Dimension(400,500));
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
-        return f;
-    }
-    JPanel getHPanel(){
-            JPanel panel = new JPanel();
-            panel.setLayout(new BoxLayout(panel,BoxLayout.X_AXIS));
-            panel.setMaximumSize(new Dimension(Integer.MAX_VALUE,40));
-            panel.setPreferredSize(new Dimension(Integer.MAX_VALUE,40));
-            return panel;
+    static class smaliParser{
+        String mFilePath = "";
+        List<String> lines;
+        smaliParser(String filepath){
+            System.out.println("Started...");
+            mFilePath = filepath;
+            try {
+                lines = Files.readAllLines(Paths.get(mFilePath));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if(lines.size()==0){return;}
+            String expect = null;
+            int startline = 0;
+            String type = "";
+            int index = 0;
+            boolean optional = false;
+            String st = ".field,.method,.super,.source,.interface,.implements,.class";
+            for(String line:lines){
+                index ++; //starts from 1
+                line = line.trim();
+                if(line.equals("")){continue;}
+                String[] words = line.split("\\s");
+                if(expect!=null){
+                    if(line.startsWith(expect)){
+                        printBlock(type,startline,index);
+                        expect = null;
+                        type = null;
+                        startline = 0;
+                        continue;
+                    }else if(optional && st.contains(words[0] + ",")){
+                        printBlock(type,startline,startline);
+                        expect = null;
+                        type = null;
+                        startline = 0;
+                    }
+                }
+                switch (words[0]){
+                    case ".method" -> {
+                        expect = ".end method";
+                        type="method";
+                        optional = false;
+                        startline=index;
+                    }
+                    case ".field" -> {
+                        expect = ".end field";
+                        type="field";
+                        optional = true;
+                        startline=index;
+                    }
+                    case ".annotation" -> {
+                        if(expect==null) { //because it is also internal directive.
+                            expect = ".end annotation";
+                            type = "annotation";
+                            optional = false;
+                            startline = index;
+                        }
+                    }
+                    case ".class" -> {printBlock("class",index,index);}
+                    case ".super" -> {printBlock("extends",index,index);}
+                    case ".source" -> {printBlock("source",index,index);}
+                    case ".implements" -> {printBlock("implements",index,index);}
+                    case ".interface" -> {printBlock("interface",index,index);}
+                    default -> {
+                        if(expect==null) {
+                            if(!line.startsWith("#")) {
+                                System.out.println("UnExpected Word " + words[0]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void printBlock(String name,int start,int end){
+            /*
+            * Errors:
+            * 1. More than one occurance of .class etc.
+            * 2. Method filed name special characters.
+            *
+            *
+            * */
+            System.out.println("===" + name + " start:" + start + "  end: " + end);
+        }
     }
 }
